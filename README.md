fb1bae17ca890435a458b00bc73c6ddd981b223a
FrankScarlet <hferfrank@gmail.com>
2021-01-08 20:55:04 +0800

    format & add readme

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # Py-book
 
-## 运行方式
+## 运行工具及环境
 
-### VS Code (推荐)
+> 两个维护者的开发工具并不一致
 
-安装Python插件(待补充格式化)，选择好运行环境后，F5就能执行单个程序。
+### IDLE + PyCharm used by gzlgfang
 
-### IDLE
+- 用IDLE打开文件，F5运行Module的方式执行程序 
 
-用IDLE打开文件，F5运行Module的方式执行程序 
+### VS Code used by FrankScarlet
+
+VS Code插件如下：
+
+- Python `ms-python.python` 
+
+配置文件里追加`[python]`默认语法格式化器选项（用的默认`autopep8`），`Ctrl+Shift+P`输入`Format Document`的时候指定使用微软的Python插件格式化`.py`文件
+
+- Jupyter `ms-toolsai.jupyter`
+
+可装可不装，这本书里应该不涉及`.ipynb`，不过即使不装这个插件上面那个插件似乎也支持打开笔记本。
+
+- Markdown All in One `yzhang.markdown-all-in-one`
+
+最近发现这个插件还挺好用的。比VS Code原本默认的Markdown支持更高一点。
 
 
 ## 目录
--- solve/xxfc2.py
+++ solve/xxfc2.py
@@ -1,17 +1,15 @@
 from scipy import linalg as la
 from numpy import *
 
-a=mat([[1,2,4],[3,5,6],[12,7,9]])#输入方程系数
-b=mat([[4],[11],[24]])#输入方程右边常数项
-x1=la.solve(a,b)
-x2=a.I*b
-x1=x1.T #转置
-x2=x2.T
-n=len(x1[0])#求第1行的元素个数
+a = mat([[1, 2, 4], [3, 5, 6], [12, 7, 9]])  # 输入方程系数
+b = mat([[4], [11], [24]])  # 输入方程右边常数项
+x1 = la.solve(a, b)
+x2 = a.I*b
+x1 = x1.T  # 转置
+x2 = x2.T
+n = len(x1[0])  # 求第1行的元素个数
 for i in range(n):
-    print('x1_',i+1,'=',x1[0,i]) #第1种方法求解
+    print('x1_', i+1, '=', x1[0, i])  # 第1种方法求解
 
 for i in range(n):
-    print('x2_',i+1,'=',x2[0,i]) #第2种方法求解
-
-
+    print('x2_', i+1, '=', x2[0, i])  # 第2种方法求解
